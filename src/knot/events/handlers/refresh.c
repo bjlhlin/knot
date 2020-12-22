d9af773b0b891b8b0efd8971a7c79959d98d7a1f
David Vašek <david.vasek@nic.cz>
2020-12-22 16:54:49 +0100

    refresh: if refresh isn't supported or fails, return an error

--- src/knot/events/handlers/refresh.c
+++ src/knot/events/handlers/refresh.c
@@ -1236,7 +1236,7 @@ int event_refresh(conf_t *conf, zone_t *zone)
 	assert(zone);
 
 	if (!zone_is_slave(conf, zone)) {
-		return KNOT_EOK;
+		return KNOT_ENOTSUP;
 	}
 
 	try_refresh_ctx_t trctx = { 0 };
@@ -1280,11 +1280,11 @@ int event_refresh(conf_t *conf, zone_t *zone)
 		zone->timers.next_refresh = now + max_refresh;
 	}
 
-	/* Rechedule events. */
+	/* Reschedule events. */
 	replan_from_timers(conf, zone);
 	if (trctx.send_notify) {
 		zone_events_schedule_at(zone, ZONE_EVENT_NOTIFY, time(NULL) + 1);
 	}
 
-	return KNOT_EOK;
+	return ret;
 }
